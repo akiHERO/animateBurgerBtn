41fe9a9a2644e7d8dea65e8abf98ea76085cf242
akiHERO <atakada01@gmail.com>
2016-01-03 14:13:29 +0900

    first commit

--- animateBurgerBtn/ViewController.swift
+++ animateBurgerBtn/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var button: HamburgerButton! = nil
+    let size = UIScreen.mainScreen().bounds
 
+    
+    @IBOutlet weak var posText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red: 38.0 / 255, green: 151.0 / 255, blue: 68.0 / 255, alpha: 1)
+        
+        self.button = HamburgerButton(frame: CGRectMake(self.size.width/2 - 27, self.size.width/2 - 27, 54, 54))
+        self.button.addTarget(self, action: "toggle:", forControlEvents:.TouchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle  {
+        return .LightContent
+    }
+    
+    func toggle(sender: AnyObject!) {
+        self.button.showsMenu = !self.button.showsMenu
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func posBtn(sender: UIButton) {
 
+    }
+    @IBAction func posSlider(sender: UISlider) {
+        posText.text = String(sender.value)
+        self.button.animateWithRatio(sender.value)
+    }
 
 }
 
